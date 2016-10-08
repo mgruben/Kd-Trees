d33cba8203409409aaa26f60fc3a9f43ec787ac1
mgruben <GrubenM@gmail.com>
2016-10-08 10:59:58 -0500

    Copy PointSET API from assignment

--- PointSET.java
+++ PointSET.java
@@ -20,5 +20,14 @@
  * @author Michael <GrubenM@GMail.com>
  */
 public class PointSET {
-    
+   public         PointSET()                               // construct an empty set of points 
+   public           boolean isEmpty()                      // is the set empty? 
+   public               int size()                         // number of points in the set 
+   public              void insert(Point2D p)              // add the point to the set (if it is not already in the set)
+   public           boolean contains(Point2D p)            // does the set contain point p? 
+   public              void draw()                         // draw all points to standard draw 
+   public Iterable<Point2D> range(RectHV rect)             // all points that are inside the rectangle 
+   public           Point2D nearest(Point2D p)             // a nearest neighbor in the set to point p; null if the set is empty 
+
+   public static void main(String[] args)                  // unit testing of the methods (optional) 
 }
